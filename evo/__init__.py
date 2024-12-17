9948ca035ff1ec01a6b7e3b2b66bfa89ec12fe02
adititm <adititm@stanford.edu>
2024-12-17 10:41:26 -0800

    Update generation code and add semantic_mining folder with configs, prompts, and scripts.

--- evo/__init__.py
+++ evo/__init__.py
@@ -4,3 +4,7 @@ from .models import Evo
 
 from .generation import generate
 from .scoring import score_sequences, positional_entropies
+
+from .models import Evo, MODEL_NAMES, load_checkpoint
+
+__all__ = ['Evo', 'MODEL_NAMES', 'load_checkpoint']
